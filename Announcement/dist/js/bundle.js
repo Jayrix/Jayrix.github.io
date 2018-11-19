e19b092caf15320965d1fb04e5ed18aef86f37fa
Jakub Kozlik <kozlik.jakub@gmail.com>
2018-11-19 18:07:59 +0100

    more style imprvs

--- Announcement/dist/js/bundle.js
+++ Announcement/dist/js/bundle.js
@@ -501,7 +501,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 document.addEventListener('DOMContentLoaded', function () {
     (0, _reactDom.render)(_react2.default.createElement(_Root2.default, null), document.getElementById('root'));
 
-    setInterval(function () {
+    setTimeout(function () {
         window.location.reload(true);
     }, 7200000);
 });
