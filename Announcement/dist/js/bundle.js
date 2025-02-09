5398a7920921e286de6ba2857a66fd8c50483b9a
Jakub Kozlik <kozlik.jakub@gmail.com>
2025-02-10 00:28:11 +0100

    1.80 live

--- Announcement/dist/js/bundle.js
+++ Announcement/dist/js/bundle.js
@@ -498,7 +498,7 @@ var _Root2 = _interopRequireDefault(_Root);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var isOnline = __webpack_require__(41);
+var isOnline = __webpack_require__(43);
 
 //zmienne konfiguracyjne odswiezania
 var GET_URL = "https://jayrix.github.io/Announcement/";
@@ -23736,51 +23736,59 @@ var _Recepty = __webpack_require__(28);
 
 var _Recepty2 = _interopRequireDefault(_Recepty);
 
-var _Rejestracja = __webpack_require__(29);
+var _TrzeciaSzczepionka = __webpack_require__(29);
+
+var _TrzeciaSzczepionka2 = _interopRequireDefault(_TrzeciaSzczepionka);
+
+var _Maseczki = __webpack_require__(30);
+
+var _Maseczki2 = _interopRequireDefault(_Maseczki);
+
+var _Rejestracja = __webpack_require__(31);
 
 var _Rejestracja2 = _interopRequireDefault(_Rejestracja);
 
-var _Kardiolog = __webpack_require__(30);
+var _Kardiolog = __webpack_require__(32);
 
 var _Kardiolog2 = _interopRequireDefault(_Kardiolog);
 
-var _GabinetNeurologiczny = __webpack_require__(31);
+var _GabinetNeurologiczny = __webpack_require__(33);
 
 var _GabinetNeurologiczny2 = _interopRequireDefault(_GabinetNeurologiczny);
 
-var _GabinetChirurgiiDzieciecej = __webpack_require__(32);
+var _GabinetChirurgiiDzieciecej = __webpack_require__(34);
 
 var _GabinetChirurgiiDzieciecej2 = _interopRequireDefault(_GabinetChirurgiiDzieciecej);
 
-var _PracowniaUSG = __webpack_require__(33);
+var _PracowniaUSG = __webpack_require__(35);
 
 var _PracowniaUSG2 = _interopRequireDefault(_PracowniaUSG);
 
-var _PoradniaKardiologiczna = __webpack_require__(34);
+var _PoradniaKardiologiczna = __webpack_require__(36);
 
 var _PoradniaKardiologiczna2 = _interopRequireDefault(_PoradniaKardiologiczna);
 
-var _HPV = __webpack_require__(35);
+var _HPV = __webpack_require__(37);
 
 var _HPV2 = _interopRequireDefault(_HPV);
 
-var _ZleceniaRecept = __webpack_require__(36);
+var _ZleceniaRecept = __webpack_require__(38);
 
 var _ZleceniaRecept2 = _interopRequireDefault(_ZleceniaRecept);
 
-var _Ktomalek = __webpack_require__(37);
+var _Ktomalek = __webpack_require__(39);
 
 var _Ktomalek2 = _interopRequireDefault(_Ktomalek);
 
-var _PoradniaDietetyczna = __webpack_require__(38);
+var _PoradniaDietetyczna = __webpack_require__(40);
 
 var _PoradniaDietetyczna2 = _interopRequireDefault(_PoradniaDietetyczna);
 
-var _SzczepionkiGrypa = __webpack_require__(39);
+var _SzczepionkiGrypa = __webpack_require__(41);
 
 var _SzczepionkiGrypa2 = _interopRequireDefault(_SzczepionkiGrypa);
 
-var _AktualizacjaInfo = __webpack_require__(40);
+var _AktualizacjaInfo = __webpack_require__(42);
 
 var _AktualizacjaInfo2 = _interopRequireDefault(_AktualizacjaInfo);
 
@@ -23794,8 +23802,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 //import CovidInfo from "./CovidInfo.jsx";
 
 //import TestNaPrzeciwciala from "./TestNaPrzeciwciala.jsx";
-//import TrzeciaSzczepionka from "./TrzeciaSzczepionka.jsx";
-//import Maseczki from "./Maseczki.jsx";
+
 //import WstrzymanieSzczepien from "./WstrzymanieSzczepien.jsx";
 //import ProgramSzczepien from "./ProgramSzczepien.jsx";
 //import Maseczki_Ukraina from "./Maseczki_Ukraina.jsx";
@@ -23847,7 +23854,7 @@ var AnnouncementList = function (_Component) {
         var _this = _possibleConstructorReturn(this, (AnnouncementList.__proto__ || Object.getPrototypeOf(AnnouncementList)).call(this, props));
 
         _this.state = {
-            announcements: [_react2.default.createElement(_AktualizacjaInfo2.default, null), _react2.default.createElement(_SzczepionkiGrypa2.default, null), _react2.default.createElement(_PoradniaDietetyczna2.default, null), _react2.default.createElement(_Ktomalek2.default, null), _react2.default.createElement(_HPV2.default, null), _react2.default.createElement(_ZleceniaRecept2.default, null), _react2.default.createElement(_PoradniaKardiologiczna2.default, null), _react2.default.createElement(_GabinetNeurologiczny2.default, null), _react2.default.createElement(_SzczepieniaCovid2.default, null), _react2.default.createElement(_Rejestracja2.default, null), _react2.default.createElement(_Kardiolog2.default, null), _react2.default.createElement(_PracowniaUSG2.default, null), _react2.default.createElement(_Recepty2.default, null), _react2.default.createElement(_Profilaktyka40plus2.default, null), _react2.default.createElement(_Antiviral2.default, null), _react2.default.createElement(_ZleceniaRecept2.default, null), _react2.default.createElement(_GabinetChirurgiiDzieciecej2.default, null), _react2.default.createElement(_SzczepieniaCovid2.default, null), _react2.default.createElement(_Medea2.default, null), _react2.default.createElement(_DrFiedor2.default, null), _react2.default.createElement(_Podomedis2.default, null), _react2.default.createElement(_CR2.default, null)].concat(geers_array, psp_array),
+            announcements: [_react2.default.createElement(_Maseczki2.default, null), _react2.default.createElement(_TrzeciaSzczepionka2.default, null), _react2.default.createElement(_AktualizacjaInfo2.default, null), _react2.default.createElement(_SzczepionkiGrypa2.default, null), _react2.default.createElement(_PoradniaDietetyczna2.default, null), _react2.default.createElement(_Ktomalek2.default, null), _react2.default.createElement(_HPV2.default, null), _react2.default.createElement(_ZleceniaRecept2.default, null), _react2.default.createElement(_PoradniaKardiologiczna2.default, null), _react2.default.createElement(_GabinetNeurologiczny2.default, null), _react2.default.createElement(_SzczepieniaCovid2.default, null), _react2.default.createElement(_Rejestracja2.default, null), _react2.default.createElement(_Kardiolog2.default, null), _react2.default.createElement(_PracowniaUSG2.default, null), _react2.default.createElement(_Recepty2.default, null), _react2.default.createElement(_Profilaktyka40plus2.default, null), _react2.default.createElement(_Antiviral2.default, null), _react2.default.createElement(_TrzeciaSzczepionka2.default, null), _react2.default.createElement(_AktualizacjaInfo2.default, null), _react2.default.createElement(_ZleceniaRecept2.default, null), _react2.default.createElement(_GabinetChirurgiiDzieciecej2.default, null), _react2.default.createElement(_SzczepieniaCovid2.default, null), _react2.default.createElement(_Medea2.default, null), _react2.default.createElement(_DrFiedor2.default, null), _react2.default.createElement(_Podomedis2.default, null), _react2.default.createElement(_CR2.default, null)].concat(geers_array, psp_array),
             movedLeft: false
 
             //buffer array for sliding
@@ -24698,6 +24705,129 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var TrzeciaSzczepionka = function TrzeciaSzczepionka(props) {
+
+    return _react2.default.createElement(
+        "div",
+        null,
+        _react2.default.createElement(
+            "article",
+            { className: "TrzeciaSzczepionkaContent" },
+            _react2.default.createElement(
+                "div",
+                { className: "imageContainer" },
+                _react2.default.createElement("img", { src: "./dist/img/trzeciaSzczepionka/osteoporoza.jpg", alt: "osteoporoza", title: "osteoporoza" })
+            ),
+            _react2.default.createElement(
+                "strong",
+                { id: "title" },
+                "BADANIE NA OSTEOPOROZ\u0118"
+            ),
+            _react2.default.createElement(
+                "p",
+                { className: "TrzeciaSzczepionkaText" },
+                _react2.default.createElement(
+                    "span",
+                    null,
+                    "W dniu 28.03.2025"
+                ),
+                " odb\u0119d\u0105 si\u0119 badania ",
+                _react2.default.createElement(
+                    "strong",
+                    null,
+                    "osteoporozy"
+                ),
+                " w osteobusie na naszym parkingu. Zapisy i szczeg\xF3\u0142y w rejestracji."
+            )
+        )
+    );
+};
+
+exports.default = TrzeciaSzczepionka;
+
+/***/ }),
+/* 30 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Maseczki = function Maseczki(props) {
+
+    return _react2.default.createElement(
+        "div",
+        null,
+        _react2.default.createElement(
+            "article",
+            { className: "maseczkiContent" },
+            _react2.default.createElement(
+                "div",
+                { className: "imageContainer" },
+                _react2.default.createElement("img", { src: "./dist/img/maseczki/maska.jpg", alt: "maska", title: "maska" })
+            ),
+            _react2.default.createElement(
+                "strong",
+                { id: "title" },
+                "UWAGA!"
+            ),
+            _react2.default.createElement(
+                "p",
+                { className: "maseczkiText" },
+                "Bardzo prosimy osoby z infekcj\u0105 o noszenie maseczek."
+            )
+        )
+    );
+};
+
+/*
+const Maseczki = (props) => {
+    
+    return(
+        
+        <div>
+            <article className="maseczkiContent">
+                <div className="imageContainer">
+                    <img src="./dist/img/maseczki/maska.jpg" alt="maska" title="maska"/>
+                </div>
+                <strong id="title">OBOWIĄZEK NOSZENIA MASECZEK</strong>
+                <p className="maseczkiText">
+                    Zawiadamiamy, że w związku z obowiązujacym prawem w obiektach medycznych (w tym w poradniach) <strong>nadal należy nosić maseczki zakrywające nos i usta.</strong>
+                </p>
+            </article>
+        </div>
+        
+    )
+}
+*/
+exports.default = Maseczki;
+
+/***/ }),
+/* 31 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var Rejestracja = function Rejestracja(props) {
 
     return _react2.default.createElement(
@@ -24723,7 +24853,7 @@ var Rejestracja = function Rejestracja(props) {
 exports.default = Rejestracja;
 
 /***/ }),
-/* 30 */
+/* 32 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24769,7 +24899,7 @@ var Kardiolog = function Kardiolog(props) {
 exports.default = Kardiolog;
 
 /***/ }),
-/* 31 */
+/* 33 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24822,7 +24952,7 @@ var GabinetNeurologiczny = function GabinetNeurologiczny(props) {
 exports.default = GabinetNeurologiczny;
 
 /***/ }),
-/* 32 */
+/* 34 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24875,7 +25005,7 @@ var GabinetChirurgiiDzieciecej = function GabinetChirurgiiDzieciecej(props) {
 exports.default = GabinetChirurgiiDzieciecej;
 
 /***/ }),
-/* 33 */
+/* 35 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24981,7 +25111,7 @@ var PracowniaUSG = function PracowniaUSG(props) {
 exports.default = PracowniaUSG;
 
 /***/ }),
-/* 34 */
+/* 36 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25077,7 +25207,7 @@ var PoradniaKardiologiczna = function PoradniaKardiologiczna(props) {
 exports.default = PoradniaKardiologiczna;
 
 /***/ }),
-/* 35 */
+/* 37 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25129,7 +25259,7 @@ var HPV = function HPV(props) {
 exports.default = HPV;
 
 /***/ }),
-/* 36 */
+/* 38 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25170,7 +25300,7 @@ var ZleceniaRecept = function ZleceniaRecept(props) {
 exports.default = ZleceniaRecept;
 
 /***/ }),
-/* 37 */
+/* 39 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25228,7 +25358,7 @@ var Ktomalek = function Ktomalek(props) {
 exports.default = Ktomalek;
 
 /***/ }),
-/* 38 */
+/* 40 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25324,7 +25454,7 @@ var PoradniaDietetyczna = function PoradniaDietetyczna(props) {
 exports.default = PoradniaDietetyczna;
 
 /***/ }),
-/* 39 */
+/* 41 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25376,7 +25506,7 @@ var SzczepionkiGrypa = function SzczepionkiGrypa(props) {
 exports.default = SzczepionkiGrypa;
 
 /***/ }),
-/* 40 */
+/* 42 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25419,13 +25549,13 @@ var AktualizacjaInfo = function AktualizacjaInfo(props) {
 exports.default = AktualizacjaInfo;
 
 /***/ }),
-/* 41 */
+/* 43 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-const publicIp = __webpack_require__(42);
+const publicIp = __webpack_require__(44);
 
 const defaults = {
 	timeout: 5000,
@@ -25439,12 +25569,12 @@ module.exports = options => {
 
 
 /***/ }),
-/* 42 */
+/* 44 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-const isIp = __webpack_require__(43);
+const isIp = __webpack_require__(45);
 
 const defaults = {
 	timeout: 5000
@@ -25490,12 +25620,12 @@ module.exports.v6 = opts => {
 
 
 /***/ }),
-/* 43 */
+/* 45 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-const ipRegex = __webpack_require__(44);
+const ipRegex = __webpack_require__(46);
 
 const isIp = module.exports = x => ipRegex({exact: true}).test(x);
 isIp.v4 = x => ipRegex.v4({exact: true}).test(x);
@@ -25503,7 +25633,7 @@ isIp.v6 = x => ipRegex.v6({exact: true}).test(x);
 
 
 /***/ }),
-/* 44 */
+/* 46 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
